41410d38790bf119674b0cd9e812a666153be7e0
fedpre <fed.pregnolato@gmail.com>
2022-05-19 22:08:11 +0200

    Trying to fix react router natiove

--- rate-repository-app/src/components/Main.jsx
+++ rate-repository-app/src/components/Main.jsx
@@ -1,13 +1,12 @@
-import { StyleSheet } from "react-native";
-import theme from "../theme";
 import AppBar from "./AppBar";
 import RepositoryList from "./RepositoryList";
+
 
 const Main = () => {
   return (
     <>
       <AppBar/>
-      <RepositoryList/>
+      <RepositoryList />
     </>
   );
 };
